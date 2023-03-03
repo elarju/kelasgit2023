9d494c8f49a0d4520f0ccc1a434d70987fc9bb5d
elarju <arju.academy28@gmail.com>
2023-03-03 15:26:58 +0700

    v1.3 mengecek program bilangan looping 1-n dengan menampilkan bilangan ganjil

--- tugas2.py
+++ tugas2.py
@@ -3,3 +3,12 @@ print("Program bilangan bulat 1 hingga n")
 n = int(input("Masukkan bilangan bulat: "))
 for i in range(n):
   print(i+1)
+  
+#Soal no 2
+print("Program bilangan ganjil 1 hingga n")
+n = int(input("Masukkan bilangan bulat: "))
+for i in range(n+1):
+  if(i%2 == 0):
+    continue
+  else:
+    print(i)
